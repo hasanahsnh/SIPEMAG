b46ad997a8a1fbefad5ffe75addfa48fe3b92391
DindaDiva <dindadiva61881@gmail.com>
2024-09-25 11:26:16 +0800

    menyelesaikan tampilan login

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -19,17 +19,84 @@
     <link href="{{ asset('assets/css/tabler-payments.min.css?1692870487') }}" rel="stylesheet"/>
     <link href="{{ asset('assets/css/tabler-vendors.min.css?1692870487') }}" rel="stylesheet"/>
     <link href="{{ asset('assets/css/demo.min.css?1692870487') }}" rel="stylesheet"/>
+
     <style>
       @import url('https://rsms.me/inter/inter.css');
       :root {
       	--tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
       }
-      body {
-      	font-feature-settings: "cv03", "cv04", "cv11";
-        background-image: url('{{ asset('assets/css/img/bg-img.jpg') }}');
-        background-size: cover;
-      }
+        body {
+          font-feature-settings: "cv03", "cv04", "cv11";
+          background-image: url('{{ asset('assets/css/img/bg-img.jpg') }}');
+          background-size: cover;
+          background-position: center;
+        }
+
+          body::before {
+                content: "";
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-color:#1E2A5E;
+                opacity: 30%;
+                z-index: 1;
+                pointer-events: none;
+              }
+
+          body * {
+              position: relative;
+              z-index: 2;
+              }
+
+        .card {
+          border-radius:30px;
+          margin-left:20px;
+          margin-right:20px;
+          background-color:#EEEEEE;
+
+        }
+
+        .button {
+          float: right;
+          padding: 10px 30px;
+          background-color:#1E2A5E;
+          color:white;
+          border-radius:50px;
+          cursor:pointer;
+          border:none;
+          font-weight:700;
+        }
+
+       
+
+        .form-control{
+            border-radius:50px;
+            background-color:#55679C;
+            opacity: 30%;
+        }
+
+        .input-group-text{
+            border-radius:50px;
+            background-color:#55679C;
+        }
+
+        h1{
+          font-size:40px;
+          font-weight:700;
+          padding:0 0 20px 0;
+          color:#1E2A5E;
+        }
+
+        .form-label{
+          padding:0 0 0 15px;
+          font-weight:700;
+          color:#1E2A5E;
+        }
+
     </style>
+    
   </head>
   <body  class=" d-flex flex-column">
     <script src="{{ asset('assets/js/demo-theme.min.js?1692870487') }}"></script>
@@ -38,28 +105,19 @@
         
         <div class="card card-md">
           <div class="card-body">
-            <h1 class="h2 text-center mb-4">User Login</h1>
+            <h1 class="text-center">User Login</h1>
             <form action="./" method="get" autocomplete="off" novalidate>
               <div class="mb-3">
                 <label class="form-label">Username</label>
-                <input type="email" class="form-control" placeholder="Your Username" autocomplete="off">
+                <input type="email" class="form-control"  autocomplete="off">
               </div>
               <div class="mb-2">
-                <label class="form-label">
-                  Password
-                </label>
-                <div class="input-group input-group-flat">
-                  <input type="password" class="form-control"  placeholder="Your Password"  autocomplete="off">
-                  <span class="input-group-text">
-                    <a href="#" class="link-secondary" title="Show password" data-bs-toggle="tooltip"><!-- Download SVG icon from http://tabler-icons.io/i/eye -->
-                      <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" /><path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" /></svg>
-                    </a>
-                  </span>
-                </div>
+                <label class="form-label">Password</label>
+                <input type="password" class="form-control"  autocomplete="off">
               </div>
              
               <div class="form-footer">
-                <button type="submit" class="btn btn-primary w-100">Login</button>
+                <button class="button" type="submit">Login</button>
               </div>
             </form>
           </div>
