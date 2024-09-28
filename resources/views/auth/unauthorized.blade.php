2b37189c6b4e5e49e673561d53dcf434164f52cb
hasanahsnh <sanah.hasanah0204@gmail.com>
2024-09-28 23:13:46 +0800

    membuat fungsi un/authorized

---
+++ resources/views/auth/unauthorized.blade.php
@@ -0,0 +1,113 @@
+<!doctype html>
+<!--
+* Tabler - Premium and Open Source dashboard template with responsive and high quality UI.
+* @version 1.0.0-beta20
+* @link https://tabler.io
+* Copyright 2018-2023 The Tabler Authors
+* Copyright 2018-2023 codecalm.net Paweł Kuna
+* Licensed under MIT (https://github.com/tabler/tabler/blob/master/LICENSE)
+-->
+<html lang="en">
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover"/>
+    <meta http-equiv="X-UA-Compatible" content="ie=edge"/>
+    <title>Login - SIPEMAG</title>
+    <!-- CSS files -->
+    <link href="{{ asset('assets/css/tabler.min.css?1692870487') }}" rel="stylesheet"/>
+    <link href="{{ asset('assets/css/tabler-flags.min.css?1692870487') }}" rel="stylesheet"/>
+    <link href="{{ asset('assets/css/tabler-payments.min.css?1692870487') }}" rel="stylesheet"/>
+    <link href="{{ asset('assets/css/tabler-vendors.min.css?1692870487') }}" rel="stylesheet"/>
+    <link href="{{ asset('assets/css/demo.min.css?1692870487') }}" rel="stylesheet"/>
+
+    <style>
+      @import url('https://rsms.me/inter/inter.css');
+      :root {
+      	--tblr-font-sans-serif: 'Inter Var', -apple-system, BlinkMacSystemFont, San Francisco, Segoe UI, Roboto, Helvetica Neue, sans-serif;
+      }
+        body {
+          font-feature-settings: "cv03", "cv04", "cv11";
+          background-image: url('{{ asset('assets/img/login/bg-img.jpg') }}');
+          background-size: cover;
+          background-position: center;
+        }
+
+          body::before {
+                content: "";
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background-color:#1E2A5E;
+                opacity: 30%;
+                z-index: 1;
+                pointer-events: none;
+              }
+
+          body * {
+              position: relative;
+              z-index: 2;
+              }
+
+        .card {
+          border-radius:30px;
+          margin-left:20px;
+          margin-right:20px;
+          background-color:#EEEEEE;
+
+        }
+
+        .button {
+          float: right;
+          padding: 10px 30px;
+          background-color:#1E2A5E;
+          color:white;
+          border-radius:50px;
+          cursor:pointer;
+          border:none;
+          font-weight:700;
+        }
+
+        .form-control{
+            border-radius:50px;
+            background-color:#55679C;
+            opacity: 30%;
+        }
+
+        .input-group-text{
+            border-radius:50px;
+            background-color:#55679C;
+        }
+
+        h1{
+          font-size:40px;
+          font-weight:700;
+          padding:0 0 20px 0;
+          color:#1E2A5E;
+        }
+
+        .form-label{
+          padding:0 0 0 15px;
+          font-weight:700;
+          color:#1E2A5E;
+        }
+
+    </style>
+    
+  </head>
+  <body  class=" d-flex flex-column">
+    <script src="{{ asset('assets/js/demo-theme.min.js?1692870487') }}"></script>
+    <!-- Code Here -->
+    <div class="container mt-5">
+      <div class="alert alert-danger">
+          <h4 class="alert-heading">Akses Ditolak</h4>
+      </div>
+    </div>
+    
+    <!-- Libs JS -->
+    <!-- Tabler Core -->
+    <script src="{{ asset('assets/js/tabler.min.js?1692870487') }}" defer></script>
+    <script src="{{ asset('assets/js/demo.min.js?1692870487') }}" defer></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,24 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SesiController;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\UnauthorizedController;
+use App\Http\Middleware\CheckRole;
 
 Route::get('/', [SesiController::class, 'index']);
 Route::post('/', [SesiController::class, 'login']);
 Route::get('/logout', [SesiController::class, 'logout']);
+Route::get('/unauthorized', [UnauthorizedController::class, 'index']);
 
-Route::get('/administrator', [AdminController::class, 'index']);
+Route::middleware(['auth', CheckRole::class.':admin'])->group(function() {
+    // Hanya admin yang dapat mengakses halaman:
+    Route::get('/administrator', [AdminController::class, 'index']);
+});
 
+/*Route::middleware(['auth', CheckRole::class.':admin'])->group(function() {
+    // Hanya pemagang yangdapat mengakses halaman:
+});
+
+Route::middleware(['auth', CheckRole::class.':admin'])->group(function() {
+    // Hanya Dosen, guru pembimbing dan atau pembimbing lapangan yang dapat mengakses halaman:
+});*/
 
